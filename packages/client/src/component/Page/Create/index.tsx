333420df6022f81213d2894890c73ca0dd52c1d8
Henry Blanchette <henry@monkeyface.com>
2023-06-14 17:54:39 +0800

    staged deck creation screen

--- packages/client/src/component/Page/Create/index.tsx
+++ packages/client/src/component/Page/Create/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './style.css'
 import * as Page from '../../Page'
 import * as Controller from '../../Controller'
@@ -7,16 +7,111 @@ type Props = {
 
 }
 
+type State = {
+  stage: Stage
+}
+
+type Stage
+  = { case: 'awaiting input' }
+  | { case: 'generating', input: string }
+  | { case: 'generated' }
+
 export const Create: React.FC<Props> = (props) => {
   let { controllerState, setControllerState } = useContext(Controller.GlobalContext)
 
+  let [state, setState] = useState<State>({
+    stage: { case: 'awaiting input' }
+  })
+
   return (
     <Page.Page name="create">
-      <button onClick={event => {
-        console.log("go to page: menu")
-        setControllerState(Controller.setPage('menu')(controllerState))
-      }}>Menu</button>
-      <div>This is the "create" page</div>
+      <div className='create-page-inner'>
+        <button onClick={event => {
+          console.log("go to page: menu")
+          setControllerState(Controller.setPage('menu')(controllerState))
+        }}>Menu</button>
+
+        <h2>AI-Powered Deck Generator</h2>
+
+        <p>Describe a theme, and the AI will generate a deck of cards that are inspired by it.</p>
+
+        {(() => {
+          switch (state.stage.case) {
+            case 'awaiting input': return (
+              <div className='create-form'>
+                <textarea className='create-prompt' id='create-promptTextarea' placeholder="your deck's theme" />
+                <button className='create-form-submit'
+                  onClick={event => {
+                    let _promptTextarea = document.getElementById('create-promptTextarea')
+                    if (_promptTextarea === null) { throw new Error("promptTextarea is null") }
+                    let promptTextarea: HTMLTextAreaElement = _promptTextarea as HTMLTextAreaElement
+                    let value: string = promptTextarea.value
+                    console.log("promptTextarea.value: ", value)
+                    setState({ ...state, stage: { case: 'generating', input: value } })
+                  }}
+
+                // disabled={promptTextarea.props.value.trim() === ""}
+                >Submit</button>
+              </div>
+            )
+            case 'generating': return (
+              <div className='create-generating'>
+                <p>Generating a deck with the theme:</p>
+                <p className='create-generating-theme'>{state.stage.input}</p>
+                <div className='create-generating-loading'>
+                  <div className="create-generating-loading-center">
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                    <div className="create-generating-loading-wave"></div>
+                  </div>
+                </div>
+              </div>
+            )
+            case 'generated': return (
+              <div className='create-result'></div>
+            )
+          }
+        })()}
+      </div>
     </Page.Page >
   )
 }
