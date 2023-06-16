5d7a189d85289f1b6d831c51e97dc281d5d22d77
Henry Blanchette <henry@monkeyface.com>
2023-06-16 14:54:11 +0800

    delete bad files

--- packages/client/src/ai/generate_image.ts
+++ packages/client/src/ai/generate_image.ts
@@ -2,7 +2,7 @@ import { sleep } from "@latticexyz/utils";
 import crypto_bro_png from "../public/crypto_bro.png";
 
 import * as Generation from "./generation/generation_pb";
-import { GenerationServiceClient } from "./generation/generation_pb_service";
+import {GenerationServiceClient} from "./generation/generation_pb_service";
 import { grpc as GRPCWeb } from "@improbable-eng/grpc-web";
 import { NodeHttpTransport } from "@improbable-eng/grpc-web-node-http-transport";
 
@@ -11,7 +11,7 @@ GRPCWeb.setDefaultTransport(NodeHttpTransport());
 
 // Authenticate using your API key, don't commit your key to a public repository!
 const metadata = new GRPCWeb.Metadata();
-metadata.set("Authorization", "Bearer " + process.env.API_KEY);
+metadata.set("Authorization", "Bearer " + process.env.dreamstudio_api_key);
 
 // Create a generation client to use with all future requests
 const client = new GenerationServiceClient("https://grpc.stability.ai", {});
