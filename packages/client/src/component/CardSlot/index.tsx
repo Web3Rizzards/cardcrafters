d9fda64b51e269575f089743f79de1404f20e4bb
Kenk <kenghin_lim@hotmail.com>
2023-06-16 20:15:12 +0800

    update card storage

--- packages/client/src/component/CardSlot/index.tsx
+++ packages/client/src/component/CardSlot/index.tsx
@@ -2,12 +2,17 @@ import "./style.css";
 
 import * as game from "../../data/game";
 
+import Card from "../Card";
 import React from "react";
 
-type Props = { onClick: () => void };
+type Props = { onClick: () => void; card?: game.Card };
 
-const CardSlot: React.FC<Props> = ({ onClick }) => (
-  <div onClick={onClick} className="card-slot"></div>
-);
+const CardSlot: React.FC<Props> = ({ onClick, card }) => {
+  if (!card) {
+    return <div onClick={onClick} className="card-slot"></div>;
+  } else {
+    return <Card card={card}> </Card>;
+  }
+};
 
 export default CardSlot;
--- packages/client/src/component/Page/Play/index.tsx
+++ packages/client/src/component/Page/Play/index.tsx
@@ -44,8 +44,14 @@ export const Play: React.FC<Props> = (props) => {
   const players = useComponentValue(Game, singletonEntity);
 
   const [name, setName] = useState("");
-  const [cardSlot, setCardSlot] = useState(0);
-  const [target, setTarget] = useState(0);
+  const [player1FieldIndex, setPlayer1FieldIndex] = useState(0);
+  const [player2FieldIndex, setPlayer2FieldIndex] = useState(0);
+
+  const [player1Card, setPlayer1Card] = useState(0);
+  const [player2Card, setPlayer2Card] = useState(0);
+
+  // Given the entity ids get player card details
+  function getPlayerCards(): game.Card[] {}
 
   return (
     <Page.Page name="play">
@@ -65,6 +71,7 @@ export const Play: React.FC<Props> = (props) => {
         <div className="game-center">
           {/* Opponent's hand */}
           <div className="game-hand game-opponentHand">
+            {}
             <Card card={game.exampleCard1}></Card>
             <Card card={game.exampleCard1}></Card>
             <Card card={game.exampleCard1}></Card>
@@ -80,6 +87,7 @@ export const Play: React.FC<Props> = (props) => {
             <Card card={game.exampleCard1}></Card>
           </div>
 
+          {/* Player 1 Stats */}
           <div className="game-stats">
             <div className="health-bar">
               <div
@@ -92,103 +100,80 @@ export const Play: React.FC<Props> = (props) => {
             <div className="game-stats-mana">0 / 10</div>
           </div>
 
-          {/* Opponent's field */}
+          {/* Player 1 field */}
           <div className="game-field game-opponentField">
-            <CardSlot onClick={() => setTarget(0)}></CardSlot>
-            <CardSlot onClick={() => setTarget(1)}></CardSlot>
-            <CardSlot onClick={() => setTarget(2)}></CardSlot>
-            <CardSlot onClick={() => setTarget(3)}></CardSlot>
-            <CardSlot onClick={() => setTarget(4)}></CardSlot>
-          </div>
-          <div className="game-fieldDivider"></div>
-
-          {/* Player's field */}
-          <div className="game-field game-playerField">
             <CardSlot
               onClick={() => {
-                setCardSlot(0);
+                setPlayer1FieldIndex(0);
                 console.log("XXX");
               }}
             ></CardSlot>
-            <CardSlot onClick={() => setCardSlot(1)}></CardSlot>
-            <CardSlot onClick={() => setCardSlot(2)}> </CardSlot>
-            <CardSlot onClick={() => setCardSlot(3)}></CardSlot>
-            <CardSlot onClick={() => setCardSlot(4)}></CardSlot>
+            <CardSlot onClick={() => setPlayer1FieldIndex(1)}></CardSlot>
+            <CardSlot onClick={() => setPlayer1FieldIndex(2)}> </CardSlot>
+            <CardSlot onClick={() => setPlayer1FieldIndex(3)}></CardSlot>
+            <CardSlot onClick={() => setPlayer1FieldIndex(4)}></CardSlot>
+          </div>
+          <div className="game-fieldDivider"></div>
+
+          {/* Player 2 field */}
+          <div className="game-field game-playerField">
+            <CardSlot
+              onClick={() => setPlayer2FieldIndex(0)}
+              card={game.exampleCard1}
+            ></CardSlot>
+            <CardSlot onClick={() => setPlayer2FieldIndex(1)}></CardSlot>
+            <CardSlot onClick={() => setPlayer2FieldIndex(2)}></CardSlot>
+            <CardSlot onClick={() => setPlayer2FieldIndex(3)}></CardSlot>
+            <CardSlot onClick={() => setPlayer2FieldIndex(4)}></CardSlot>
           </div>
 
+          {/* Player 2 Stats */}
           <div className="game-stats">
-            <div className="game-stats-health">30</div>
+            <div className="health-bar">
+              <div
+                className="health-bar-fill"
+                style={{ width: (30 / 30) * 100 + "%" }}
+              >
+                <span className="health-bar-text">{30}</span>
+              </div>
+            </div>
             <div className="game-stats-mana">0 / 10</div>
           </div>
 
-          {/* Player's hand */}
+          {/* Player 2 hand */}
           <div className="game-hand game-playerHand">
             <Card card={game.exampleCard1}></Card>
             <Card card={game.exampleCard1}></Card>
           </div>
         </div>
         <div className="game-sidebar game-rightSidebar">
-          {/* <button>Join Player 1</button>
-          <button>Join Player 2</button>
-          <button>Forfeit</button>
-          <button>Claim Victory</button>
-          <button>Attack</button>
-          <button>End Turn</button> */}
-
           <div>
-            <div>
-              Card Name
-              <input
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-              ></input>
-            </div>
-            <div>
-              Card Slot (0 to 4)
-              {cardSlot}
-              {/* <input
-                onChange={(e) => {
-                  setCardSlot(Number(e.target.value));
-                }}
-              ></input> */}
-            </div>
-            <div>
-              Target (0 to 4)
-              {target}
-              {/* <input
-                onChange={(e) => {
-                  setTarget(Number(e.target.value));
-                }}
-              ></input> */}
-            </div>
+            <div>Player 1 Field Index (0 to 4): {player1FieldIndex}</div>
+            <div>Player 2 Field Index (0 to 4): {player2FieldIndex}</div>
 
-            <div>Player 1 Health:</div>
-            <div>Player 2 Health:</div>
-            {/* <Button onClick={() => createCard(name)}>Create Card</Button> */}
             <Button onClick={() => joinPlayer1()}>
               {players?.player1 ? players?.player1 : "Join player 1"}
             </Button>
             <Button onClick={() => joinPlayer2()}>
               {players?.player2 ? players?.player2 : "Join player 2"}
             </Button>
-            <Button onClick={() => summonCard(name, Number(cardSlot))}>
+            <Button onClick={() => summonCard(name, Number(player1FieldIndex))}>
               Summon Card
             </Button>
-            {/* <Button onClick={() => endTurn(name, Number(cardSlot))}>
+            {/* <Button onClick={() => endTurn(name, Number(player1FieldIndex))}>
               End Turn
             </Button>
-            <Button onClick={() => forfeit(name, Number(cardSlot))}>
+            <Button onClick={() => forfeit(name, Number(player1FieldIndex))}>
               Forfeit
             </Button>
-            <Button onClick={() => forfeitOpponent(name, Number(cardSlot))}>
+            <Button onClick={() => forfeitOpponent(name, Number(player1FieldIndex))}>
               Forfeit Opponent
             </Button>
-            <Button onClick={() => win(name, Number(cardSlot))}>Win</Button>
-            <Button onClick={() => attack(name, Number(cardSlot))}>
+            <Button onClick={() => win(name, Number(player1FieldIndex))}>Win</Button>
+            <Button onClick={() => attack(name, Number(player1FieldIndex))}>
               Attack
             </Button>
-            <Button onClick={() => attackPlayer(name, Number(cardSlot))}>
+            <Button onClick={() => attackPlayer(name, Number(player1FieldIndex))}>
               Attack Player
             </Button> */}
           </div>
